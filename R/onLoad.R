3b81308e9dd20a9e36fcc2990d93060b84e7e87c
Kurt Hornik <Kurt.Hornik@R-project.org>
2011-04-09 08:29:19 +0000

    version 0.4-7

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,13 @@
 .onLoad <-
 function(libname, pkgname)
+{
     .jpackage(pkgname, lib.loc = libname)
+    ## Weka's dynamic class discovery (used by e.g. the Snowball stemmer
+    ## class to locate the snowball jar file in the classpath) triggers
+    ## the loading of Weka's packages and makes the KnowledgeFlow
+    ## rebuild its internal set of components, causing trouble on
+    ## systems without AWT, unless we do the following.
+    if(nzchar(Sys.getenv("NOAWT")))
+        .jcall("java/lang/System", "S",
+               "setProperty", "java.awt.headless", "true")
+}
